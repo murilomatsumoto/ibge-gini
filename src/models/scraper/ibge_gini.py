8926ff6649e7d95ea144399ef46b6331647e2324
murilomatsumoto <murilomatsumoto@gmail.com>
2024-04-02 14:48:46 -0400

    feat: scraping até a página de download

--- src/models/scraper/ibge_gini.py
+++ src/models/scraper/ibge_gini.py
@@ -2,6 +2,9 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
 def main():
     options = Options()
@@ -15,8 +18,18 @@ def main():
     if menu:
         print('menu encontrado')
         menu.click()
+        sleep(1)
+        estatisticas_element = driver.find_element(By.XPATH, '//li[@idmenu="1" and @class="parent nvcls_"]')
+        if estatisticas_element:
+            estatisticas_element.click()
+            downloads_element = driver.find_element(By.XPATH, '//li[@idmenu="36" and @class=" nvcls_"]')
+            if downloads_element:
+                downloads_element.click()
+                
+                WebDriverWait(driver, 10).until(
+                     EC.presence_of_element_located((By.CLASS_NAME, "conteudo__interna__titulo pure-u-1")))
+        
     
-
     driver.quit()
 
 if __name__ == "__main__":
