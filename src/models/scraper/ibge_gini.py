b094a4cee06f8a2013d95c33eeb70cb01e1f0b7f
murilomatsumoto <murilomatsumoto@gmail.com>
2024-04-02 15:46:05 -0400

    feat: encontrando os arquivos para download

--- src/models/scraper/ibge_gini.py
+++ src/models/scraper/ibge_gini.py
@@ -27,9 +27,34 @@ def main():
                 downloads_element.click()
                 
                 WebDriverWait(driver, 10).until(
-                     EC.presence_of_element_located((By.CLASS_NAME, "conteudo__interna__titulo pure-u-1")))
-        
-    
+                    EC.url_contains('downloads')
+                )
+                        
+    censo_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'Censos_anchor'))     
+            )
+                    
+    censo_element = driver.find_element(By.ID, 'Censos_anchor')
+    if censo_element:
+        censo_element.click()
+        censo_noventa_um_element = driver.find_element(By.XPATH, '//li[@aria-labelledby="Censos/Censo_Demografico_1991_anchor"]')
+        if censo_noventa_um_element:
+            censo_noventa_um_element.click()
+            sleep(5)
+            indice_gini_element = driver.find_element(By.XPATH, '//li[@aria-labelledby="Censos/Censo_Demografico_1991/Indice_de_Gini_anchor"]/i')
+
+            if indice_gini_element:
+                try:
+                    indice_gini_element.click()    
+                except:
+                    driver.execute_script('arguments[0].click();', indice_gini_element) 
+                    print('ok')
+                estados_tag = driver.find_elementindice_gini_element = driver.find_element(By.XPATH, '//li[@aria-labelledby="Censos/Censo_Demografico_1991/Indice_de_Gini_anchor"]/ul')
+                if estados_tag:
+                    itens_download = estados_tag.find_elements(By.TAG_NAME, 'li')
+                    for item in itens_download:
+                        print(item.text)
+                    
     driver.quit()
 
 if __name__ == "__main__":
